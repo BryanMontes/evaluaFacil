3f973021af35b8aa0e27326efbb9ac11e51113fc
Bryan Montes <bryanmontes@MacBook-Pro-de-Bryan.local>
2015-11-09 00:44:26 -0600

    Se hace funcional la pantalla de listar docentes, quedando pendiente apis de, elimiar docente y recuperar contraseña

--- resources/views/docentes/listado.blade.php
+++ resources/views/docentes/listado.blade.php
@@ -2,15 +2,10 @@
 
 @section('css')
 
-<!-- BEGIN PAGE LEVEL STYLES -->
-{!!Html::style('librerias/assets/global/plugins/select2/select2.css')!!}
-{!!Html::style('librerias/assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.css')!!}
-<!-- END PAGE LEVEL STYLES -->
 <!-- BEGIN THEME STYLES -->
 {!!Html::style('librerias/assets/global/css/components-md.css')!!}
-{!!Html::style('librerias/assets/global/css/plugins-md.css')!!}
-{!!Html::style('librerias/assets/admin/layout/css/layout.css')!!}
-{!!Html::style('librerias/assets/admin/layout/css/custom.css')!!}
+
+
 @stop
 
 @section('contenedor')
@@ -22,48 +17,46 @@
         <div class="col-md-12">
             <!-- BEGIN EXAMPLE TABLE PORTLET-->
             <div class="portlet box blue">
-
                 <div class="portlet-body">
-                    <table class="table table-striped table-hover table-bordered" id="sample_editable_1">
+                    <table class="table table-striped table-hover table-bordered" ng-cloak="">
                         <thead>
                             <tr>
-                                <th>
-                                    Nombre(s)
+                                <th class="col-md-2"><a href="" ng-click="reverse = !reverse;
+                                            order('first_name', reverse)">Nombre(s) &nbsp;<i class="fa fa-sort"></i></a>
                                 </th>
-                                <th>
-                                    Apellido Paterno
-                                </th>
-                                <th>
-                                    Usuario
-                                </th>
-                                <th>
-                                    Asignaciones
-                                </th>
-                                <th> 
-                                    Borrar
-                                </th>
+                                <th class="col-md-2"><a href="" ng-click="reverse = !reverse;
+                                    order('last_name', reverse)">Apellido Paterno &nbsp;<i class="fa fa-sort"></i></a></th>
+                                <th class="col-md-2"><a href="" ng-click="reverse = !reverse;
+                                    order('user.username', reverse)">Usuario &nbsp;<i class="fa fa-sort"></i></a></th>
+                                <th class="col-md-3"><a href="" ng-click="reverse = !reverse;
+                                    order('age', reverse)">Asignaciones &nbsp;<i class="fa fa-sort"></i></a></th>
+                                <th class="col-md-3">Opciones</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr ng-repeat="docente in listarDocentes" ng-if="listarDocentes.length>0">
+                        <tbody  ng-show="listarDocente.length > 0">
+                            <tr ng-repeat="docente in listarDocente">
                                 <td>
-                                    @{{docente.nombre}}
+                                    @{{docente.first_name}}
                                 </td>
                                 <td>
-                                    @{{docente.apellido}}
+                                    @{{docente.last_name}}
                                 </td>
                                 <td>
-                                    @{{docente.usuario}}
+                                    @{{docente.user.username}}
                                 </td>
                                 <td class="center">
                                     @{{docente.asignaciones}}
                                 </td>
                                 <td>
-                                    <a class="delete" href="javascript:;">
-                                        Delete </a>
+                                    <a class="delete" style="color: red;" href="javascript:;" ng-click="borrarDocente(docente.id)">
+                                        <i class="fa fa-trash-o"></i> Eliminar&nbsp;&nbsp;</a>
+                                    <a class="delete" href="javascript:;" ng-click="editarDocente(docente.id)">
+                                        <i class="fa fa-pencil"></i> Editar&nbsp;&nbsp;</a>
+                                    <a class="delete" href="javascript:;" ng-click="recuperarPasswordDocente(docente.id)">
+                                        <i class="fa fa-retweet"></i> Recuperar Clave</a>
                                 </td>
                             </tr>
-                            
+
                         </tbody>
                     </table>
                 </div>
@@ -73,58 +66,160 @@
         <div>
             <script type="text/ng-template" id="agregarDocente">
                 <div class="modal-header">
-                    <h3 class="modal-title">Registrar Docente</h3>
-                </div>
+                <h3 class="modal-title">Registrar Docente</h3>
+                </div> 
                 <div class="modal-body">
-                    <div class="col-md-12 pad-inputs">
-                        <label class="col-md-2">Nombre:</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" ng-model="docente.nombre">
-                        </div>
-                    </div>
-                    <div class="col-md-12 pad-inputs">
-                        <label class="col-md-2">Apellidos:</label>
-                        <div class="col-md-10">
-                            <input type="text" class="col-md-12 form-control" ng-model="docente.apellido">
-                        </div>
-                    </div>
-                    <div class="col-md-12 pad-inputs">
-                        <label class="col-md-2">Email:</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" ng-model="docente.email">
-                        </div>
-                    </div>
-                    <div class="col-md-12 pad-inputs">
-                        <label class="col-md-2">Teléfono:</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" ng-model="docente.telefono">
-                        </div>
-                    </div>
-                    <div class="col-md-12 pad-inputs">
-                        <label class="col-md-2">Usuario:</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" ng-model="docente.usuario">
-                        </div>
-                    </div>
-                    <div class="col-md-12 pad-inputs">
-                        <label class="col-md-2">Contraseña:</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" ng-model="docente.contrasena">
-                        </div>
-                    </div>
-                        <div class="col-md-12 pad-inputs">
-                        <label class="col-md-2">Confirmar Contraseña:</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" ng-model="docente.confirmContrasena">
-                        </div>
-                    </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2">Nombre:</label>
+                <div class="col-md-10">
+                <input type="text" class="form-control" ng-model="docente.nombre" maxlength="20">
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2">Apellidos:</label>
+                <div class="col-md-10">
+                <input type="text" class="col-md-12 form-control" ng-model="docente.apellido" maxlength="20">
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2">Email:</label>
+                <div class="col-md-10">
+                <input type="email" class="form-control" ng-model="docente.email">
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2">Teléfono:</label>
+                <div class="col-md-10">
+                <input type="text" class="form-control" ng-model="docente.telefono" maxlength="10">
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2">Usuario:</label>
+                <div class="col-md-10">
+                <input type="text" class="form-control" ng-model="docente.usuario" maxlength="20">
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2 control-label" for="Rol">Rol:</label>
+                <div class="col-md-10">
+                <select id="Rol" name="Rol" class="form-control" ng-model="docente.rol">
+                <option value="teacher">Maestro</option>
+                <option value="director">Director</option>
+                </select>
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2">Contraseña:</label>
+                <div class="col-md-10">
+                <input type="password" class="form-control" ng-model="docente.contrasena" maxlength="10">
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2">Confirmar Contraseña:</label>
+                <div class="col-md-10">
+                <input type="password" class="form-control" ng-model="docente.confirmContrasena" maxlength="10">
+                </div>
+                </div>
+
                 </div>
                 <div class="modal-footer">
-                    <button class="btn btn-primary" type="button" ng-click="guardar()">Guardar</button>
-                    <button class="btn btn-warning" type="button" ng-click="cancel()">Cancelar</button>
+                <button class="btn btn-primary" type="button" ng-click="guardar()">Guardar</button>
+                <button class="btn btn-warning" type="button" ng-click="cancel()">Cancelar</button>
+                <ul ng-show="erroresInsertarDocente.length > 0" class="alert alert-danger text-center" style="margin-top: 10px;" ng-cloak>
+                <li ng-repeat="error in erroresInsertarDocente">@{{error.tipoError}}</li>
+                </ul>
                 </div>
             </script>
-        </div>  
+        </div> 
+        <div>
+            <script type="text/ng-template" id="editarDocente">
+                <div class="modal-header">
+                <h3 class="modal-title">Editar Docente</h3>
+                </div> 
+                <div class="modal-body">
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2">Nombre:</label>
+                <div class="col-md-10">
+                <input type="text" class="form-control" ng-model="editar.first_name" maxlength="20">
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2">Apellidos:</label>
+                <div class="col-md-10">
+                <input type="text" class="col-md-12 form-control" ng-model="editar.last_name" maxlength="20">
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2">Email:</label>
+                <div class="col-md-10">
+                <input type="email" class="form-control" ng-model="editar.email">
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-2">Teléfono:</label>
+                <div class="col-md-10">
+                <input type="text" class="form-control" ng-model="editar.contact_number" maxlength="10">
+                </div>
+                </div>
+
+
+                </div>
+                <div class="modal-footer">
+                <button class="btn btn-primary" type="button" ng-click="guardarEditar()">Guardar Cambios</button>
+                <button class="btn btn-warning" type="button" ng-click="cancel()">Cancelar</button>
+                <ul ng-show="erroresInsertarDocente.length > 0" class="alert alert-danger text-center" style="margin-top: 10px;" ng-cloak>
+                <li ng-repeat="error in erroresInsertarDocente">@{{error.tipoError}}</li>
+                </ul>
+                </div>
+            </script>
+        </div>
+        <div>
+            <script type="text/ng-template" id="recuperarPasswordDocente">
+                <div class="modal-header">
+                <h3 class="modal-title">Reinicializar Contraseña</h3>
+                </div> 
+                <div class="modal-body">
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-3">Nombre:</label>
+                <div class="col-md-9">
+                <input type="text" class="form-control" ng-model="recuperarPassword.first_name" maxlength="20" disabled="">
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                <label class="col-md-3">Apellidos:</label>
+                <div class="col-md-9">
+                <input type="text" class="col-md-12 form-control" ng-model="recuperarPassword.last_name" maxlength="20" disabled="">
+                </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                    <label class="col-md-3">Nombre de usuario:</label>
+                    <div class="col-md-9">
+                        <input type="email" class="form-control" ng-model="recuperarPassword.user.username" disabled="">
+                    </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                    <label class="col-md-3">Nueva Contraseña:</label>
+                    <div class="col-md-9">
+                        <input type="password" class="form-control" ng-model="recuperarPassword.nuevoPassword" maxlength="10">
+                    </div>
+                </div>
+                <div class="col-md-12 pad-inputs">
+                    <label class="col-md-3">Confirmar Contraseña:</label>
+                    <div class="col-md-9">
+                        <input type="password" class="form-control" ng-model="recuperarPassword.confirmarPassword" maxlength="10">
+                    </div>
+                </div>
+
+                </div>
+                <div class="modal-footer">
+                <button class="btn btn-primary" type="button" ng-click="recuperarPasswordDocente()">Guardar Cambios</button>
+                <button class="btn btn-warning" type="button" ng-click="cancel()">Cancelar</button>
+                <ul ng-show="erroresInsertarDocente.length > 0" class="alert alert-danger text-center" style="margin-top: 10px;" ng-cloak>
+                <li ng-repeat="error in erroresInsertarDocente">@{{error.tipoError}}</li>
+                </ul>
+                </div>
+            </script>
+        </div>
     </div>
 </div>
 
@@ -136,16 +231,7 @@
 
 
 <!-- BEGIN PAGE LEVEL PLUGINS -->
-{!!Html::script('librerias/assets/global/plugins/select2/select2.min.js')!!}
-{!!Html::script('librerias/assets/global/plugins/datatables/media/js/jquery.dataTables.min.js')!!}
-{!!Html::script('librerias/assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js')!!}
-{!!Html::script('librerias/assets/admin/pages/scripts/table-editable.js')!!}
-<script>
-    jQuery(document).ready(function () {
-        Metronic.init(); // init metronic core components
-        TableEditable.init();
-    });
-</script>
+
 {!!Html::script('js/docentes/listado.js')!!}
 
 @stop
